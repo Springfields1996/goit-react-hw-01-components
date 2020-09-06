b42ff6cfbbd63098ae160b25edd6abde560e3e92
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-09-06 22:17:39 +0300

    Updates

---
+++ precache-manifest.d81131fc2aecc99955c004b543cd908f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886ccb0466b71c3d6d9e3bfb382bfd42",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "d41b9db0267b70f8328c",
+    "url": "/goit-react-hw-01-components/static/css/main.961c295b.chunk.css"
+  },
+  {
+    "revision": "7d4670fedc2d75545252",
+    "url": "/goit-react-hw-01-components/static/js/2.f3b95e16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.f3b95e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41b9db0267b70f8328c",
+    "url": "/goit-react-hw-01-components/static/js/main.423cda3f.chunk.js"
+  },
+  {
+    "revision": "f9b103e158dc2827622f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.d31938df.js"
+  }
+]);
